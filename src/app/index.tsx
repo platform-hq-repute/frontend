97a4a3b66b1ca3cdb46890a36657c1e00357c00e
platform-hq-repute <platform@reputehq.com>
2026-03-19 00:06:52 +0000

    Delete src directory

--- src/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-
-// Mobile users are redirected to the tabs layout
-// Web users see the landing page (index.web.tsx)
-export default function Index() {
-  return <Redirect href="/(tabs)" />;
-}
